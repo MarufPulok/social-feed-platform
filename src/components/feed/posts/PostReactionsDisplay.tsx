7012398ee2098a854bd00ac6d8a6b1df6ee4c5f0
kernelpanic <maruf12@student.sust.edu>
2025-11-23 01:44:29 +0600

    Update PostReactionsDisplay component to increase reaction image sizes and apply consistent styling for improved visual presentation.

--- src/components/feed/posts/PostReactionsDisplay.tsx
+++ src/components/feed/posts/PostReactionsDisplay.tsx
@@ -9,36 +9,41 @@ export default function PostReactionsDisplay() {
           src="/assets/images/react_img1.png"
           alt="Reaction"
           className="_react_img1"
-          width={24}
-          height={24}
+          width={32}
+          height={32}
+          style={{ borderRadius: "50%", objectFit: "cover" }}
         />
         <Image
           src="/assets/images/react_img2.png"
           alt="Reaction"
           className="_react_img"
-          width={24}
-          height={24}
+          width={32}
+          height={32}
+          style={{ borderRadius: "50%", objectFit: "cover" }}
         />
         <Image
           src="/assets/images/react_img3.png"
           alt="Reaction"
           className="_react_img _rect_img_mbl_none"
-          width={24}
-          height={24}
+          width={32}
+          height={32}
+          style={{ borderRadius: "50%", objectFit: "cover" }}
         />
         <Image
           src="/assets/images/react_img4.png"
           alt="Reaction"
           className="_react_img _rect_img_mbl_none"
-          width={24}
-          height={24}
+          width={32}
+          height={32}
+          style={{ borderRadius: "50%", objectFit: "cover" }}
         />
         <Image
           src="/assets/images/react_img5.png"
           alt="Reaction"
           className="_react_img _rect_img_mbl_none"
-          width={24}
-          height={24}
+          width={32}
+          height={32}
+          style={{ borderRadius: "50%", objectFit: "cover" }}
         />
         <p className="_feed_inner_timeline_total_reacts_para">9+</p>
       </div>
