818150c425dbd82e9d363b79cd7f00ccc64feab4
kernelpanic <maruf12@student.sust.edu>
2025-11-22 21:13:30 +0600

    Implement LoginForm component in login page for improved user experience

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,187 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function LoginForm() {
+  return (
+    <section className="_social_login_wrapper _layout_main_wrapper">
+      <div className="_shape_one">
+        <Image
+          src="/svg/shape1.svg"
+          alt=""
+          className="_shape_img"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/svg/dark_shape.svg"
+          alt=""
+          className="_dark_shape"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="_shape_two">
+        <Image
+          src="/svg/shape2.svg"
+          alt=""
+          className="_shape_img"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/svg/dark_shape1.svg"
+          alt=""
+          className="_dark_shape _dark_shape_opacity"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="_shape_three">
+        <Image
+          src="/svg/shape3.svg"
+          alt=""
+          className="_shape_img"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/svg/dark_shape2.svg"
+          alt=""
+          className="_dark_shape _dark_shape_opacity"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="_social_login_wrap">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
+              <div className="_social_login_left">
+                <div className="_social_login_left_image">
+                  <Image
+                    src="/assets/images/login.png"
+                    alt="Image"
+                    className="_left_img"
+                    width={800}
+                    height={600}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+              <div className="_social_login_content">
+                <div className="_social_login_left_logo _mar_b28">
+                  <Image
+                    src="/svg/logo.svg"
+                    alt="Image"
+                    className="_left_logo"
+                    width={150}
+                    height={50}
+                  />
+                </div>
+                <p className="_social_login_content_para _mar_b8">
+                  Welcome back
+                </p>
+                <h4 className="_social_login_content_title _titl4 _mar_b50">
+                  Login to your account
+                </h4>
+                <button
+                  type="button"
+                  className="_social_login_content_btn _mar_b40"
+                >
+                  <Image
+                    src="/svg/google.svg"
+                    alt="Image"
+                    className="_google_img"
+                    width={20}
+                    height={20}
+                  />{" "}
+                  <span>Or sign-in with google</span>
+                </button>
+                <div className="_social_login_content_bottom_txt _mar_b40">
+                  {" "}
+                  <span>Or</span>
+                </div>
+                <form className="_social_login_form">
+                  <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control _social_login_input"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control _social_login_input"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="form-check _social_login_form_check">
+                        <input
+                          className="form-check-input _social_login_form_check_input"
+                          type="radio"
+                          name="flexRadioDefault"
+                          id="flexRadioDefault2"
+                          defaultChecked
+                        />
+                        <label
+                          className="form-check-label _social_login_form_check_label"
+                          htmlFor="flexRadioDefault2"
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="_social_login_form_left">
+                        <p className="_social_login_form_left_para">
+                          Forgot password?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12 col-md-12 col-xl-12 col-sm-12">
+                      <div className="_social_login_form_btn _mar_t40 _mar_b60">
+                        <button
+                          type="button"
+                          className="_social_login_form_btn_link _btn1"
+                        >
+                          Login now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                <div className="row">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div className="_social_login_bottom_txt">
+                      <p className="_social_login_bottom_txt_para">
+                        Dont have an account?{" "}
+                        <Link href="#0">Create New Account</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
