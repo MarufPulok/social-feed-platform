ee1733b4b82b33cda86765377fce1e17c79b5c56
kernelpanic <maruf12@student.sust.edu>
2025-11-22 21:41:49 +0600

    Refactor FeedPage layout for improved structure and responsiveness, enhancing the organization of components and adding a custom container for better styling.

--- src/components/feed/FeedPage.tsx
+++ src/components/feed/FeedPage.tsx
@@ -1,33 +1,46 @@
-import Header from "./navigation/Header";
-import MobileHeader from "./navigation/MobileHeader";
-import MobileBottomNav from "./navigation/MobileBottomNav";
 import ThemeSwitcher from "./layout/ThemeSwitcher";
+import Header from "./navigation/Header";
+import MobileBottomNav from "./navigation/MobileBottomNav";
+import MobileHeader from "./navigation/MobileHeader";
+import Post from "./posts/Post";
+import PostComposer from "./posts/PostComposer";
 import LeftSidebar from "./sidebar/LeftSidebar";
 import RightSidebar from "./sidebar/RightSidebar";
-import StoriesSection from "./stories/StoriesSection";
 import StoriesMobile from "./stories/StoriesMobile";
-import PostComposer from "./posts/PostComposer";
-import Post from "./posts/Post";
+import StoriesSection from "./stories/StoriesSection";
 
 export default function FeedPage() {
   return (
-    <div>
+    <div className="_layout _layout_main_wrapper">
       <ThemeSwitcher />
-      <Header />
-      <MobileHeader />
-      <div>
-        <LeftSidebar />
-        <div>
-          <StoriesSection />
-          <StoriesMobile />
-          <PostComposer />
-          <Post />
-          <Post />
+      <div className="_main_layout">
+        <Header />
+        <MobileHeader />
+        <div className="container _custom_container">
+          <div className="_layout_inner_wrap">
+            <div className="row">
+              <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12">
+                <LeftSidebar />
+              </div>
+              <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                <div className="_layout_middle_wrap">
+                  <div className="_layout_middle_inner">
+                    <StoriesSection />
+                    <StoriesMobile />
+                    <PostComposer />
+                    <Post />
+                    <Post />
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12">
+                <RightSidebar />
+              </div>
+            </div>
+          </div>
         </div>
-        <RightSidebar />
+        <MobileBottomNav />
       </div>
-      <MobileBottomNav />
     </div>
   );
 }
-
